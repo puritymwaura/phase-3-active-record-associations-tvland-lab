ce05ea6093557466a0f1304f1c1b35f2fbac72c5
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-09-03 08:03:07 +0300

    done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+    has_many :actors, through: :characters
+
+    def actors_list
+        empty = []
+        self.actors.each do |act|
+            empty << "#{act.first_name} #{act.last_name}"
+        end
+        empty
+    end
 end
--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,5 +1,8 @@
 class CreateActors < ActiveRecord::Migration[6.1]
   def change
     # Your code here
+    create_tables: actors do |t|
+      t.string :first_name
+      t.integer :last_name
   end
 end
